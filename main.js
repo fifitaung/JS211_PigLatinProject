1fd25eccfc7b40c380904715230b9c6da3b19d4c
User <user@MacBook-Pro.local>
2020-05-11 02:02:17 -0500

    substring

--- main.js
+++ main.js
@@ -13,9 +13,36 @@ const rl = readline.createInterface({
 
 const pigLatin = (word) => {
 
+    if(word.match(/ /)){
+      word = word.toLowerCase().trim();
+      let wordArr = word.split(' ');
+      let result = '';
+      for (let i = 0; i < wordArr.length; i++){
+        if (i > 0){
+          result += ' ';
+        }
+        result += translate(wordArr[i]);
+      }
+      return result
+    }
+    return translate(word);
+  } 
+  const translate = (word) =>{
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+      let newWord= "";
+      if (vowels.indexOf(word[0]) > -1) {
+          newWord = word + "yay";
+          return newWord;
+      } 
+      else {
+          let firstMatch = word.match(/[aeiou]/) || 0;
+          let vowel = word.indexOf(firstMatch[0]);
+          newWord = word.substring(vowel) + word.substring(0, vowel) + "ay";
+          return newWord;
+      }
+    }
   // Your code here
 
-}
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
